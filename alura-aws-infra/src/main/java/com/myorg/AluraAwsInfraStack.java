08bba85ee17ce508c94c48678b2c2bd0b9f5997b
Matheus Melo <demelosmatheus@outlook.com>
2024-06-28 21:56:37 -0300

    curso 2 aula 03

--- alura-aws-infra/src/main/java/com/myorg/AluraAwsInfraStack.java
+++ alura-aws-infra/src/main/java/com/myorg/AluraAwsInfraStack.java
@@ -1,5 +1,10 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -14,11 +19,28 @@ public class AluraAwsInfraStack extends Stack {
     public AluraAwsInfraStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Vpc vpc = Vpc.Builder.create(this, "AluraJavaMsVpc")
+                .maxAzs(3)
+                .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AluraAwsInfraQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Cluster cluster = Cluster.Builder.create(this, "AluraJavaMsCluster")
+                .vpc(vpc).build();
+
+        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraJavaMsFargate")
+                .serviceName("alura-service-hello")
+                .cluster(cluster)
+                .cpu(256)
+                .desiredCount(1)
+                .listenerPort(8080)
+                .assignPublicIp(true)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .image(ContainerImage.fromRegistry("jacquelineoliveira/ola:1.0"))
+                                .containerPort(8080)
+                                .containerName("app_hello")
+                                .build())
+                .memoryLimitMiB(512)
+                .publicLoadBalancer(true)
+                .build();
     }
 }
